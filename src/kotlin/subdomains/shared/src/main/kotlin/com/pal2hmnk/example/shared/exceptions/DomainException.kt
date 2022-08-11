09aa7a8debdd2ed60f1a208b87416fe82b975fd0
hamanako-palpal <haruhito.aso@gmail.com>
2022-08-12 00:16:21 +0900

    add serialVersionUID

--- src/kotlin/subdomains/shared/src/main/kotlin/com/pal2hmnk/example/shared/exceptions/DomainException.kt
+++ src/kotlin/subdomains/shared/src/main/kotlin/com/pal2hmnk/example/shared/exceptions/DomainException.kt
@@ -1,3 +1,7 @@
 package com.pal2hmnk.example.shared.exceptions
 
-class DomainException : Exception()
+class DomainException : Exception() {
+    companion object {
+        private const val serialVersionUID: Long = -5156584693201249667L
+    }
+}
