d4be629e36481a9a91d3f16dc6755fb1a9d5ac47
hamanako-palpal <haruhito.aso@gmail.com>
2023-01-20 00:33:07 +0900

    fix query

--- schemas/db/permissions/migration/scripts/20220331134429_insert_init_record.sql
+++ schemas/db/permissions/migration/scripts/20220331134429_insert_init_record.sql
@@ -1,7 +1,7 @@
 -- // First migration.
 -- Migration SQL that makes the change goes here.
-INSERT INTO permissions (id, name) VALUES (1000, "getShopInfo");
-INSERT INTO role_permissions (role_key, permission_id) VALUES ("guest", 1000);
+INSERT INTO permissions (id, name) VALUES (1000, 'getShopInfo');
+INSERT INTO role_permissions (role_key, permission_id) VALUES ('guest', 1000);
 -- //@UNDO
 -- SQL to undo the change goes here.
 DELETE FROM permissions WHERE id = 1000;
