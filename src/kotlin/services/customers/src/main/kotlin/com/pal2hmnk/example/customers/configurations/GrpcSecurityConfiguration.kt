340f5b80d78eb11fb9142aeb7be0ffc8de48792b
hamanako-palpal <haruhito.aso@gmail.com>
2023-02-05 14:26:59 +0900

    add permissive endpoint

--- src/kotlin/services/customers/src/main/kotlin/com/pal2hmnk/example/customers/configurations/GrpcSecurityConfiguration.kt
+++ src/kotlin/services/customers/src/main/kotlin/com/pal2hmnk/example/customers/configurations/GrpcSecurityConfiguration.kt
@@ -2,6 +2,11 @@ package com.pal2hmnk.example.customers.configurations
 
 import com.pal2hmnk.example.customers.domains.entities.UserIdentityResolver
 import com.pal2hmnk.example.customers.securities.ExampleIdentifiedToken
+import com.pal2hmnk.example.generated.grpc.services.AuthServiceGrpcKt
+import com.pal2hmnk.example.generated.grpc.services.UserServiceGrpcKt
+import io.grpc.BindableService
+import io.grpc.ServerServiceDefinition
+import org.lognet.springboot.grpc.GRpcServicesRegistry
 import org.lognet.springboot.grpc.security.GrpcSecurity
 import org.lognet.springboot.grpc.security.GrpcSecurityConfigurerAdapter
 import org.springframework.context.annotation.Configuration
@@ -11,25 +16,41 @@ import org.springframework.security.oauth2.server.resource.BearerTokenAuthentica
 
 @Configuration
 class GrpcSecurityConfiguration(
+    private val gRpcServicesRegistry: GRpcServicesRegistry,
     private val userIdentityResolver: UserIdentityResolver,
 ): GrpcSecurityConfigurerAdapter() {
+
+    private val authenticationProvider = object : AuthenticationProvider {
+        override fun authenticate(authentication: Authentication): Authentication {
+            val authToken = authentication.credentials.toString()
+            return userIdentityResolver.resolve(authToken)
+                .takeIf { it.isValid }
+                ?.let { ExampleIdentifiedToken(it) }
+                ?: authentication
+        }
+
+        override fun supports(authentication: Class<*>?): Boolean {
+            return authentication == BearerTokenAuthenticationToken::class.java
+        }
+    }
+
+    private val permissiveAuthorizationMethods = listOf(
+        AuthServiceGrpcKt.authenticateMethod,
+        UserServiceGrpcKt.signUpMethod,
+    )
+
     @Throws(Exception::class)
     override fun configure(builder: GrpcSecurity) {
+        val methods = gRpcServicesRegistry.beanNameToServiceBeanMap.values
+            .map(BindableService::bindService)
+            .map(ServerServiceDefinition::getServiceDescriptor)
+            .flatMap { it.methods }
+            .filterNot { permissiveAuthorizationMethods.contains(it) }
+            .toTypedArray()
         builder.authorizeRequests()
-            .anyMethod().authenticated()
+            .methods(*methods)
+            .authenticated()
             .and()
-            .authenticationProvider(object : AuthenticationProvider {
-                override fun authenticate(authentication: Authentication): Authentication {
-                    val authToken = authentication.credentials.toString()
-                    return userIdentityResolver.resolve(authToken)
-                        .takeIf { it.isValid }
-                        ?.let { ExampleIdentifiedToken(it) }
-                        ?: authentication
-                }
-
-                override fun supports(authentication: Class<*>?): Boolean {
-                    return authentication == BearerTokenAuthenticationToken::class.java
-                }
-            })
+            .authenticationProvider(authenticationProvider)
     }
 }
